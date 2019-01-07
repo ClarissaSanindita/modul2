009a247435dde1592f3516034ae999cf89079f85
ClarissaSanindita <clarissa_sanindita_26rpl@student.smktelkom-mlg.sch.id>
2019-01-07 20:30:55 +0700

    selesai

---
+++ lat7.php
@@ -0,0 +1,21 @@
+<?php
+
+$arrBuah = array ("Mangga","Apel","Pisang","Jeruk"); 
+
+echo $arrBuah[0]; //untuk menampilkan []-> urutan yang dipanggil
+echo "<br>";
+echo $arrBuah[3]; //untuk menampilkan []-> urutan yang dipanggil
+echo "<br>";
+
+$arrWarna = array();
+$arrWarna[] = "Merah";
+$arrWarna[] = "Biru";
+$arrWarna[] = "Hijau";
+$arrWarna[] = "Putih";
+
+echo $arrWarna[0]; //untuk menampilkan []-> urutan yang dipanggil
+echo "<br>";
+echo $arrWarna[2]; //untuk menampilkan []-> urutan yang dipanggil
+
+
+ ?>
